412625ab6472df9df2cc25fe31ebd80f11c9fa9e
mohammad9yamani <muh.yamani.1111@gmail.com>
2024-05-22 21:18:33 +0300

    Task_16

---
+++ Tasks/Task_16.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const Task_16 = () => {
+  const [showName, setShowName] = useState(false);
+
+
+  return (
+    <View style={styles.container}>
+      <Button
+        title={showName ? "Hide" : "Show"}
+        onPress={() => {
+          setShowName(!showName);
+        }}
+      />
+      {showName && <Text style={styles.nameText}>Mohammad Al-Yamani</Text>}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  nameText: {
+    marginTop: 20,
+    fontSize: 18,
+  },
+});
+
+export default Task_16;
